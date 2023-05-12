7ed73c8d6f1638f5562a393d7d16e1d87446c156
mtownsend509 <mgtownsend509@gmail.com>
2023-05-12 11:44:10 -0700

    added volunteer and hobbies sections to about me page and simplified page a bit

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -29,35 +29,26 @@ function aboutme() {
 
     return (
     <section id="aboutMe">
-            
-        <img className ="profilePic" src= "https://raw.githubusercontent.com/mtownsend509/Portfolio-v2/main/public/images/Picture.webp" />
+    <div> 
+        <div id = 'aboutAndPic'>    
+            <img className ="profilePic" src= "https://raw.githubusercontent.com/mtownsend509/Portfolio-v2/main/public/images/Picture.webp" />
+            <p id="aboutMeTest"> About Me <br/><br/><br/>I'm a fullstack web developer based in Seattle Washington. I graduated from the University of Washington with a major in  Cellular & Molecular biology, and a minor in English. This year I was inspired to make the jump from the life sciences industry into the tech industry, so I enrolled in, and recently graduated from an edX web development bootcamp, also hosted by the University of Washington (see resume tab for development skillset).
+                Now I'm developing applications and sharpening my skills while expanding my skill set by learning Python and C#. Feel free to contact me with any coding opportunities from jobs to fun personal projects!
+            </p>
+        </div> 
         <div id="aboutList">
-            <div id="aboutTab"
-            onClick={testing}
-            >
-              <h2 id="aboutH"
-              >About</h2>
-              <h2 id="arrow"
-              >&#9662;</h2>
-            </div>
-            <p id="aboutSection" class="lastSection" className="center"> I'm a fullstack web developer based in Seattle Washington. I graduated from the University of Washington with a major in  Cellular & Molecular biology, and a minor in English. This year I was inspired to start a change in my career path, so I enrolled in, and recently graduated from an edX web development bootcamp, also hosted by the University of Washington (see resume tab for development skillset).
-            My goal for the upcoming months is to start getting on the job experience doing web development and to expand my skillset by learning Python through post class materials.
+            <p id="aboutMeHobbies">Volunteer Work <br/><br/><br/>
+            Wildlife Rehabilitation: I have volunteered at a local wildlife rehabilitation center for the past two years. My duties have included feeding and caring for injured animals, assisting with medical treatments, and helping to release animals back into the wild. I am passionate about wildlife conservation and am dedicated to helping animals in need. <br/><br/>
+Environmental Restoration: I have also volunteered with a local organization that focuses on restoring natural habitats and improving local ecosystems. This has involved planting trees and native vegetation, removing invasive species, and working to prevent erosion. I believe that it is important to protect our environment and to work towards a more sustainable future. <br/><br/>
+Disaster Response: I have participated in disaster response efforts both locally and internationally. This has included providing aid and support to communities affected by hurricanes, floods, and other natural disasters. I have received training in emergency response and am committed to helping those in need during times of crisis. <br/><br/>
             </p>
-            {/* <div id="aboutFlab"
-            onClick={testing}>
-                <h2 id="aboutH">Hobbies</h2>
-                <h2 id="arrow">&#9662;</h2>
-            </div>
-            <p id="aboutSection"> &emsp;&emsp;Just tryna test if this shit is easy
+            <p id="aboutMeHobbies" className="end">Hobbies <br/><br/><br/>
+            Playing Piano: Have you ever wanted all the benefit of hearing a 5 year old play terrible piano without all the hassel of having a kid of your own? Well hire me and I'll bring my keyboard into the office sometime! But really, playing an instrument has beneficial effects for memory and audiovisual processing. It's also a great way to relax!<br/><br/>
+Hiking: I also love spending time in nature and exploring new trails. Hiking is a great way to disconnect from technology and connect with the world around me. Whether I am hiking through the mountains or along the coast, I enjoy the physical challenge and the stunning views.<br/><br/>
+Gaming: In my free time, I also enjoy gaming. From Dungeons and Dragons to Rocket League, I find most games will offer a strategic challenge or an immersive storyline to offer a fun and unique experience. Gaming allows me to unwind and have fun while also developing problem-solving skills.<br/><br/>
             </p>
-            <div id="aboutFlab"
-            onClick={testing}>
-                <h2 id="aboutH">Volunteer Work</h2>
-                <h2 id="arrow">&#9662;</h2>
-            </div>
-            <p id="aboutSection"> &emsp;&emsp;OMG look at this man he so generous he so kind I'll definitely hire
-            </p> */}
         </div>
+        </div>   
             
     </section> 
     );
